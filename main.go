a5c879e77141eadc30faea06649583e5404cd1e8
myo-i <Imiyo0521@gmail.com>
2022-12-19 00:46:57 +0900

    ✨ SELECTでデータを取得して表示

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type Name struct {
+	FirstName string
+	LastName  string
+}
+
+// main関数を実行するには実行構成の編集でディレクトリを選択し、goファイルのパッケージ名をmainにし、main関数を作成しないといけない
+func main() {
+	fmt.Println("hello!!")
+	//connStr := "postgresql://postgres:password@::1/todos?sslmode=disable"
+
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=dvdrental sslmode=disable")
+	defer db.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rows, err := db.Query("SELECT first_name, last_name FROM actor")
+	var pp []Name
+	for rows.Next() {
+		var p Name
+		err := rows.Scan(&p.FirstName, &p.LastName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		pp = append(pp, p)
+	}
+	for _, p := range pp {
+		fmt.Println(p)
+	}
+}
