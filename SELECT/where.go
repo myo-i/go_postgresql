3fc76d33ea3f7b3309d1e63eaaa3d64635ec23be
myo-i <Imiyo0521@gmail.com>
2022-12-20 03:44:24 +0900

    【問題】

--- SELECT/where.go
+++ SELECT/where.go
@@ -6,81 +6,61 @@ import (
 	"log"
 )
 
-var email []string
-var description []string
+var emails []string
+var descriptions []string
 var phone []int
 
-func problem1() {
-
-	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=dvdrental sslmode=disable")
-	defer db.Close()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
+func problem1(db *sql.DB) {
 	cmd := "SELECT email FROM customer WHERE first_name = 'Nancy' AND last_name = 'Thomas';"
 	rows, err := db.Query(cmd)
-	for rows.Next() {
-		var p string
-		err := rows.Scan(&p)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		email = append(email, p)
-	}
-	for _, p := range email {
-		fmt.Println(p)
-	}
-}
-
-func problem2() {
-
-	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=dvdrental sslmode=disable")
-	defer db.Close()
 	if err != nil {
 		log.Fatalln(err)
 	}
+	var email string
+	addSlice(rows, emails, email)
+}
 
+func problem2(db *sql.DB) {
 	cmd := "SELECT description FROM film WHERE title ='Outlaw Hanky';"
 	rows, err := db.Query(cmd)
-	for rows.Next() {
-		var p string
-		err := rows.Scan(&p)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		description = append(description, p)
-	}
-	for _, d := range description {
-		fmt.Println(d)
-	}
-}
-
-func problem3() {
-
-	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=dvdrental sslmode=disable")
-	defer db.Close()
 	if err != nil {
 		log.Fatalln(err)
 	}
+	var description string
+	addSlice(rows, descriptions, description)
+}
 
+func problem3(db *sql.DB) {
 	cmd := "SELECT phone FROM address WHERE address = '259 Ipoh Drive';"
 	rows, err := db.Query(cmd)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var number int
+	addSlice(rows, phone, number)
+}
+
+// addSlice は取得した全業のデータをスライスに格納します
+func addSlice[T string | int](rows *sql.Rows, slice []T, row T) {
 	for rows.Next() {
-		var p int
-		err := rows.Scan(&p)
+		err := rows.Scan(&row)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		phone = append(phone, p)
+		slice = append(slice, row)
 	}
-	for _, p := range phone {
+	for _, p := range slice {
 		fmt.Println(p)
 	}
 }
 
 func ChallengeWhere() {
-	problem1()
-	problem2()
-	problem3()
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=dvdrental sslmode=disable")
+	defer db.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	problem1(db)
+	problem2(db)
+	problem3(db)
 }
