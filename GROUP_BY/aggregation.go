c8cb1aa63c69a653eedddf258ad7125730518fe5
myo-i <Imiyo0521@gmail.com>
2022-12-22 00:51:08 +0900

    ✨ 集計関数（MIN, MAX, AVG, SUM, ROUND）

---
+++ GROUP_BY/aggregation.go
@@ -0,0 +1,79 @@
+package GROUP_BY
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+type MinMax struct {
+	Min float64
+	Max float64
+}
+
+func aggMinMAX() {
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=dvdrental sslmode=disable")
+	defer db.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rows, err := db.Query("SELECT MIN(replacement_cost), MAX(replacement_cost) FROM film;")
+	//rows, err := db.Query("SELECT MIN(replacement_cost) FROM film;")
+	//rows, err := db.Query("SELECT MAX(replacement_cost) FROM film;")
+	//var pp []Name
+	for rows.Next() {
+		var p MinMax
+		err := rows.Scan(&p.Min, &p.Max)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Printf("minimum:%v, maximum:%v\n", p.Min, p.Max)
+	}
+}
+
+func aggAverageRound() {
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=dvdrental sslmode=disable")
+	defer db.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rows, err := db.Query("SELECT ROUND(AVG(replacement_cost), 2) FROM film;")
+	//var pp []Name
+	for rows.Next() {
+		var p float64
+		err := rows.Scan(&p)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(p)
+	}
+}
+
+func sum() {
+	//connStr := "postgresql://postgres:password@::1/todos?sslmode=disable"
+
+	db, err := sql.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=dvdrental sslmode=disable")
+	defer db.Close()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	rows, err := db.Query("SELECT SUM(replacement_cost) FROM film;")
+	//var pp []Name
+	for rows.Next() {
+		var p float64
+		err := rows.Scan(&p)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(p)
+	}
+}
+
+func Aggregation() {
+	aggMinMAX()
+	aggAverageRound()
+	sum()
+}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/lib/pq"
+	"go_postgresql/GROUP_BY"
 	"go_postgresql/SELECT"
 )
 
@@ -19,7 +20,12 @@ func selectStatement() {
 	SELECT.FinalChallenge()
 }
 
+func groupBy() {
+	GROUP_BY.Aggregation()
+}
+
 // main関数を実行するには実行構成の編集でディレクトリを選択し、goファイルのパッケージ名をmainにし、main関数を作成しないといけない
 func main() {
-	selectStatement()
+	//selectStatement()
+	groupBy()
 }
