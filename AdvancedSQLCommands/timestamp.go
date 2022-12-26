1a1fcbde8df1fd4caaaeb36ee37cf41ccba42393
myo-i <Imiyo0521@gmail.com>
2022-12-26 23:54:14 +0900

    ✨ timestampを使用した計算とTO_CHAR（文字列に変換してフォーマット）

--- AdvancedSQLCommands/timestamp.go
+++ AdvancedSQLCommands/timestamp.go
@@ -29,6 +29,53 @@ func timestamp1() {
 	}
 }
 
+func timestamp2() {
+	db, err := sql.Open("postgres", DataSource)
+	defer db.Close()
+	if err != nil {
+		log.Fatalln("SQL Open Error has occured", err)
+	}
+
+	// タイムスタンプから念を抽出
+	//query := "SELECT EXTRACT(YEAR FROM payment_date) AS createmonth FROM payment;"
+
+	// タイムスタンプから現在までの年月日を計算
+	query := "SELECT AGE(payment_date) FROM payment;"
+	rows, err := db.Query(query)
+	for rows.Next() {
+		var p string
+		err := rows.Scan(&p)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(p)
+	}
+}
+
+func toChar1() {
+	db, err := sql.Open("postgres", DataSource)
+	defer db.Close()
+	if err != nil {
+		log.Fatalln("SQL Open Error has occured", err)
+	}
+
+	// 年月日をフォーマットを指定して出力
+	//query := "SELECT TO_CHAR(payment_date, 'MONTH-YYYY') FROM payment;"
+	//query := "SELECT TO_CHAR(payment_date, 'mon/dd/YYYY') FROM payment;"
+	query := "SELECT TO_CHAR(payment_date, 'MM/dd/YYYY') FROM payment;"
+	rows, err := db.Query(query)
+	for rows.Next() {
+		var p string
+		err := rows.Scan(&p)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(p)
+	}
+}
+
 func TimeStamp() {
-	timestamp1()
+	//timestamp1()
+	//timestamp2()
+	toChar1()
 }
